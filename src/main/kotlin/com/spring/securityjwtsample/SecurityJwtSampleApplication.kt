9d60da264ccbe8895f88301fe6613999d8e43524
does <ldk980130@gmail.com>
2023-02-27 17:14:14 +0900

    Initial commit

---
+++ src/main/kotlin/com/spring/securityjwtsample/SecurityJwtSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.securityjwtsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurityJwtSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecurityJwtSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/spring/securityjwtsample/SecurityJwtSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.spring.securityjwtsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityJwtSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
